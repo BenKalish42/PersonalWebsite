de7a58c47d9db5a5c82a5101b8678855032eddd7
BenKalish42 <ben@djbkmusic.com>
2020-10-12 20:27:51 -0400

    forgot the icon

---
+++ src/components/graphics/menu-icon-component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ReactComponent as MenuIconSVG } from "./menu-icon.svg"
+
+
+const MenuIcon = () => (
+	<div className="bm-icon-outer-container">
+		<div className="bm-icon-inner-container">
+			<MenuIconSVG />
+		</div>
+	</div>
+)
+
+export default MenuIcon
