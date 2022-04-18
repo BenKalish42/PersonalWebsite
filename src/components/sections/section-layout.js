97c96b1817e83e20c4b4e9d28e4d0b92731f1e22
BenKalish42 <ben@djbkmusic.com>
2022-04-18 18:11:19 -0400

    changed about me photo, removed unused graphic

--- src/components/sections/section-layout.js
+++ src/components/sections/section-layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { ReactComponent as SectionGraphic } from "../graphics/section-graphic.svg"
+// import { ReactComponent as SectionGraphic } from "../graphics/section-graphic.svg"
 
 const SectionLayout = ({ children, sectionTitle }) => (
 	<div className="section-container" id={sectionTitle.replace(/\W/g, '')}>
